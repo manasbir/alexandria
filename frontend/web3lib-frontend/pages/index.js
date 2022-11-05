255aa4b057b4386bea0a82dae1f27f99c4d90d97
Horizon <omniscienthorizon@gmail.com>
2022-11-05 10:34:42 -0700

    made changes

---
+++ frontend/web3lib-frontend/pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import { ethers } from "ethers"; 
+
+const Home = () => {
+    return (
+      <h1>web3Library</h1>
+    )
+}
+
+export default Home;
+
