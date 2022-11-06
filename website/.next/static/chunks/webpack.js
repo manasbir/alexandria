26a3e9b09645035a911ba9e4b0ae4a678807a264
manas <bagrimanasbir@gmail.com>
2022-11-05 21:43:52 -0700

    a

--- website/.next/static/chunks/webpack.js
+++ website/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2f51aeccc2fba5b4"; }
+/******/ 		__webpack_require__.h = function() { return "f4ef0f2a63ea62b1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- website/pages/book/[id].js
+++ website/pages/book/[id].js
@@ -5,11 +5,15 @@ const Book = () => {
     const router = useRouter()
     const id = router.query.id
     return(
+        <div>
         <h1 style={{
             color: 'black'
         }}>
             this is book{id}
         </h1>
+
+        <iframe src="https://www.marcuswitcher.com/Euripides,%20Medea-1.pdf" ></iframe>
+         </div>
     )
 }
 
