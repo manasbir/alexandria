3eefc44c4991b7c2f7b1d95cf7e7b94832982667
manas <bagrimanasbir@gmail.com>
2022-11-05 19:49:30 -0700

    q

--- website/pages/book/[id].js
+++ website/pages/book/[id].js
@@ -1,18 +1,21 @@
 import {useRouter} from 'next/router'
 import Pdf from '../pdftest'
+import { Document } from 'react-pdf';
 
 const Book = () => {
 
     const router = useRouter()
     const id = router.query.id
     return(
+        <>
         <h1 style={{
             color: 'black'
         }}>
             this is book{id}
         </h1>
 
-        {Pdf}
+        <Document file="http://160592857366.free.fr/joe/ebooks/ShareData/The%20Republic%20by%20Plato.pdf"></Document>
+        </>
     )
 
     
