1512964bc63ee1aec23f86de8a9d9ad28fd2e05d
Kshitij Agarwal <94373821+omniscientHorizon@users.noreply.github.com>
2022-11-05 21:18:24 -0700

    fixed stupid errors

--- website/pages/book/[id].js
+++ website/pages/book/[id].js
@@ -1,28 +1,16 @@
 import {useRouter} from 'next/router'
-import { Document, Page } from 'react-pdf/renderer';
-import React, { useState } from 'react';
 
 const Book = () => {
 
-    const [numPages, setNumPages] = useState(null);
-    const [pageNumber, setPageNumber] = useState(1);
-    function onDocumentLoadSuccess({ numPages }) {
-        setNumPages(numPages);
-    }
     const router = useRouter()
     const id = router.query.id
     return(
-        <div>
-            <Document file="./The Republic by Plato.pdf" onLoadSuccess={onDocumentLoadSuccess}>
-                <Page pageNumber={pageNumber} />
-            </Document>
-            <p>
-                Page {pageNumber} of {numPages}
-            </p>
-        </div>
+        <h1 style={{
+            color: 'black'
+        }}>
+            this is book{id}
+        </h1>
     )
-
-    
 }
 
 export default Book;
